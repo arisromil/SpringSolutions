55b3e46d2aaf791390be90bec6f5c61f9b98fcd0
arisromil <arisromil@yahoo.com>
2023-05-19 20:19:47 -0600

    Set Up a Spring-WS Application

---
+++ src/java/soap/Initializer.java
@@ -0,0 +1,15 @@
+package soap;
+
+import org.springframework.ws.transport.http.support.AbstractAnnotationConfigMessageDispatcherServletInitializer;
+
+public class Initializer extends AbstractAnnotationConfigMessageDispatcherServletInitializer {
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { SpringWsConfiguration.class };
+	}
+}
---
+++ src/java/soap/SpringWsConfiguration.java
@@ -0,0 +1,17 @@
+package soap;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+@Configuration
+@EnableWs
+@ComponentScan("com.apress.spring6recipes.weather")
+public class SpringWsConfiguration {
+
+}
